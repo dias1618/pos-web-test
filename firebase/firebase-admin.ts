e2c42e9ce06a2b9be309b23f3b5678623b9a49aa
Gabriel Dias <dias.1618@gmail.com>
2022-01-29 15:21:26 -0300

    fix/firebase-credencials

--- firebase/firebase-admin.ts
+++ firebase/firebase-admin.ts
@@ -1,5 +1,4 @@
 import * as admin from 'firebase-admin';
-import FirebaseAdminSdkCredentials from './credentials-firebase-adminsdk';
 import firebase from 'firebase';
 
 export class FirebaseAdmin{
@@ -17,10 +16,28 @@ export class FirebaseAdmin{
   authAdmin;
   fire;
   
+  private convertLineBreaks(key:string) {
+    while(key.includes("\\n")){
+        key = key.replace("\\n","\n");
+    }
+    return key
+  }
+
   constructor(){
     if (!admin.apps.length) {
       admin.initializeApp({
-        credential: admin.credential.cert(JSON.parse(JSON.stringify(FirebaseAdminSdkCredentials))),
+        credential: admin.credential.cert(JSON.parse(JSON.stringify({
+          "type": "service_account",
+          "project_id": process.env.FB_PROJECT_ID,
+          "private_key_id": process.env.FB_ADMIN_PRIVATE_KEY_ID,
+          "private_key": this.convertLineBreaks(process.env.FB_ADMIN_PRIVATE_KEY.toString()),
+          "client_email": process.env.FB_ADMIN_CLIENT_EMAIL,
+          "client_id": process.env.FB_ADMIN_CLIENT_ID,
+          "auth_uri": process.env.FB_ADMIN_AUTH_URI,
+          "token_uri": process.env.FB_ADMIN_TOKEN_URI,
+          "auth_provider_x509_cert_url": process.env.FB_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+          "client_x509_cert_url": process.env.FB_ADMIN_CLIENT_X509_CERT_URL
+        }))),
         databaseURL: `https://${process.env.FB_PROJECT_ID}.firebaseio.com`,
       });
     }
